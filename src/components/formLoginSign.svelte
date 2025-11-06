fb2054e6aef217d9b9d0ff09cd9d19b97b9cd4da
1phiexdric <ruizlopezfriedrichjavier@gmail.com>
2025-11-05 22:17:39 -0400

    feat: validacion de formulario utilizando la libreria zod

--- src/components/formLoginSign.svelte
+++ src/components/formLoginSign.svelte
@@ -1,25 +1,57 @@
 <script lang="ts">
+  import {email, z} from "zod";
   import { fade } from "svelte/transition";
   let isLogin = true;
   let showPassword = false;
   function toggleForm() {
     isLogin = !isLogin;
   }
+  const registerSchema = z.object({
+    email: z.string().email({message:"Por favor, introduce un email valido"}),
+    password: z.string().min(6, {message: "La contraseña debe tener al menos 6 caracteres"}),
+    username: z.string().min(3, {message: "El nombre de usuario debe tener al menos 3 caracteres"}),
+  })
+  const loginSchema = z.object({
+    email: z.string().email({message:"Por favor, introduce un email valido"}),
+    password: z.string().min(1, {message: "La contraseña no debe estar vacia"})
+  })
 
   function togglePasswordVisibility() {
     showPassword = !showPassword;
+  }
+  let errors: Record<string, string[]> = {}
+  function submit(event: SubmitEvent){
+    event.preventDefault()
+    const formData = new FormData(event.target as HTMLFormElement)
+    // name= valor, input.value= clave
+    const data = Object.fromEntries(formData)
+    const schema = isLogin ? loginSchema : registerSchema;
+    const result = schema.safeParse(data)
+    if(!result.success){
+      errors = result.error.flatten().fieldErrors
+      console.log(errors);
+    }else{
+      errors = {}
+      console.log('!Datos validados', result.data);
+    }
   }
 </script>
 
 <div class="main">
   <h2 class="oswald">{isLogin ? "Log in" : "Registro"}</h2>
-  <form>
+  <form onsubmit={submit}>
     {#if !isLogin}
       <label for="username" transition:fade>Nombre de usuario</label>
-      <input type="text" id="username" transition:fade />
+      <input type="text" id="username" name="username" transition:fade/>
+      {#if errors.username}
+        <p class="error-message">{errors.username[0]}</p>
+      {/if}
     {/if}
     <label for="email"> Email </label>
-    <input type="email" name="email" id="email" />
+    <input type="email" name="email" id="email"/>
+    {#if errors.email}
+      <p class="error-message">{errors.email[0]}</p>
+    {/if}
     <label for="password"> password </label>
 
     <div class="input-password-container">
@@ -28,14 +60,16 @@
         name="password"
         id="password"
       />
-      <button type="button" on:click={togglePasswordVisibility}>👁</button>
+      <button type="button" onclick={togglePasswordVisibility}>👁</button>
     </div>
-
+{#if errors.password}
+      <p class="error-message">{errors.password[0]}</p>
+    {/if}
     <button class="submit" type="submit">{isLogin ? "log in" : "Sign up"}</button>
     </form>
   <p id="toggle-p">
     {isLogin ? "No" : "Ya"} tienes cuenta?
-    <button on:click={toggleForm} id="toggle-btn"
+    <button onclick={toggleForm} id="toggle-btn"
       >{isLogin ? "Registrarte" : "Log in"}</button
     >
   </p>
@@ -70,9 +104,11 @@
   .submit:hover {
     transform: scale(1.05);
   }
-  #remenber-box {
-    margin: auto;
-    cursor: pointer;
+  .error-message{
+    color: #ff4d4d;
+    font-size: 0.875rem;
+    margin-top: -5px;
+    margin-left: 20px;
   }
   #toggle-p {
     text-align: center;
